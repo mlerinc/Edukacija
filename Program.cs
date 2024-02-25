ccf8eb2ba5c7f656557e9abb062c7d2fc03e715e
mlerinc <58425174+mlerinc@users.noreply.github.com>
2024-02-25 19:08:38 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,42 +1,81 @@
-﻿using System;
-using System.Collections;
+﻿//5. ZADATAK
+////string? input = Console.ReadLine();
+////if (!string.IsNullOrEmpty(input))
+////{
+////    string toPrint = "";
+////    for ( int i = input.Length -1; i >= 0; i--)
+////    {
+////        toPrint += input[i];
+////    }
+////    Console.WriteLine(toPrint);
+////}
 
-//Array List
-public class Array
+//7. ZADATAK
+//var input = Console.ReadLine();
+
+//List<char> vowels = new List<char>()
+//{
+//    'a', 'e', 'i', 'o', 'u',
+//    'A','E', 'I', 'O', 'U'
+//};
+
+//int count = 0;
+
+//if (!string.IsNullOrEmpty(input))
+//{
+//    for (int i = 0; i < input.Length; i++)
+//    {
+//        char c = input[i];
+//        if (vowels.Contains(c))
+//        {
+//            count++;
+//        }
+//    }
+//}
+//Console.WriteLine("Input contains " + count + " vowels.");
+
+//4.ZADATAK
+//Random random = new Random();
+//int genNum = random.Next(0, 101);
+//int input;
+
+//while (true)
+//{
+//    if (int.TryParse(Console.ReadLine(), out input))
+//    {
+//        if (input < genNum)
+//        {
+//            Console.WriteLine("Prenisko, pokusajte ponovo.");
+//        }
+//        else if (input > genNum)
+//        {
+//            Console.WriteLine("Previsoko, pokusajte ponovo.");
+//        }
+//        else
+//        {
+//            Console.WriteLine("Bravo, broj je pogoden!");
+//            break;
+//        }
+//    }
+//}
+
+string path = @"E:\repos\PredavanjeCetiri\predavanjeCetiri\zadaca.txt";
+if (!File.Exists(path))
 {
-    public static void Main()
+    // Create a file to write to.
+    using (StreamWriter steamwriter = File.CreateText(path))
     {
-
-        ArrayList myArrayList = new ArrayList();
-        myArrayList.Add("Dwight");
-        myArrayList.Add("Schrute");
-        myArrayList.Add("Office");
-
-        Console.WriteLine("myArrayList");
-        Console.WriteLine(" Count: {0}", myArrayList.Count);
-        Console.WriteLine(" Capacity: {0}", myArrayList.Capacity);
-        Console.WriteLine(" IsReadOnly: {0}", myArrayList.IsReadOnly);
+        steamwriter.WriteLine("My .txt file content");
     }
 }
 
-// Sorted List
-public class SortedList
+// Open the file to read from.
+using (StreamReader steamreader = File.OpenText(path))
 {
-    public static void Main()
+    string s;
+    while ((s = steamreader.ReadLine()) != null)
     {
-        SortedList<string, string> name = new SortedList<string,string>();
-        SortedList mySortedList = new SortedList();
-        name.Add("Dwight", "Schrute");
-        name.Add("Michael", "Scott");
-        name.Add("Jim", "Halpert");
-
-        Console.WriteLine("employee of Dunder Mifflin:");
-        foreach (var pair in name)
-        {
-            Console.WriteLine($"{pair.Key} {pair.Value}");
-        }
+        Console.WriteLine(s);
     }
 }
-
-
-
+    
